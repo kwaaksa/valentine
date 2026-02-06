7bd29e258919de517f0c947ccda34ad696240563
kwaaksa <sakovichkatya2006@gmail.com>
2026-02-06 03:57:38 +0300

    Update button selectors to use new class names

--- script.js
+++ script.js
@@ -69,8 +69,8 @@ const messages = [
 let messageIndex = 0;
 
 function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
+    const noButton = document.querySelector('.нит-button');
+    const yesButton = document.querySelector('.дя-button');
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
